26ac0ac5840743e331158ca727cc8b23ac535f45
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-11 10:18:15 +0200

    Update not working

---
+++ Examples/Example01-CreatePowerPoint.ps1
@@ -0,0 +1,6 @@
+﻿Clear-Host
+Import-Module .\DocumentoZaurr.psd1 -Force
+
+$PowerPoint = New-OfficePowerPoint -FilePath $PSScriptRoot\Documents\PowerPoint.pptx
+
+Save-OfficePowerPoint -PowerPoint $PowerPoint -Show
