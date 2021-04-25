30ce564b15ac5b1ec4a07a93f54047ea53d5ba7f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-25 20:29:50 +0200

    Example

--- Examples/ExampleExcel05-ExportToExcel.ps1
+++ Examples/ExampleExcel05-ExportToExcel.ps1
@@ -1,9 +1,29 @@
 ﻿Clear-Host
 Import-Module .\PSWriteOffice.psd1 -Force
 
-New-OfficeExcel -FilePath "C:\Support\GitHub\PSWriteOffice\Examples\Documents\Excel5.xlsx" {
-    New-OfficeExcelWorkSheet -Name 'Contact54455' {
-        $Data1 = Get-Process | Select-Object -First 5
-        New-OfficeExcelTable -DataTable $Data1 -Row 1 -Column 1 -DisableAutoFilter -Theme TableStyleMedium28  #-EmphasizeFirstColumn
+$Data1 = Get-Process | Select-Object -First 100
+$Objects = @(
+    [PSCustomObject] @{ Test = 1; DateTime = (Get-Date); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'string' }
+    [PSCustomObject] @{ Test = 1; }
+    [PSCustomObject] @{ Test = 3; DateTime = (Get-Date).AddDays(1); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'string' }
+)
+
+New-OfficeExcel -FilePath "C:\Support\GitHub\PSWriteOffice\Examples\Documents\Excel.xlsx" {
+    New-OfficeExcelWorkSheet -Name 'Contact1' {
+        New-OfficeExcelTable -DataTable $Data1 -Row 1 -Column 1 -Theme TableStyleMedium28
     }
-} -Show -Save
+    New-OfficeExcelWorkSheet -Name 'Contact2' {
+        New-OfficeExcelTable -DataTable $Objects -Row 1 -Column 1 -Theme TableStyleMedium28
+    }
+    New-OfficeExcelWorkSheet -Name 'Contact3' {
+        New-OfficeExcelValue -Row 1 -Column 1 -Value 'Test1'
+        New-OfficeExcelValue -Row 1 -Column 2 -Value 'Test2'
+        New-OfficeExcelValue -Row 1 -Column 3 -Value 'Test3'
+        New-OfficeExcelValue -Row 1 -Column 4 -Value 'Test4'
+        New-OfficeExcelValue -Row 2 -Column 1 -Value 'Test5'
+        New-OfficeExcelValue -Row 2 -Column 2 -Value 'Test6'
+        New-OfficeExcelValue -Row 2 -Column 3 -Value 'Test7'
+        New-OfficeExcelValue -Row 2 -Column 4 -Value 'Test8'
+    }
+
+} -Show -Save -WhenExists Overwrite
