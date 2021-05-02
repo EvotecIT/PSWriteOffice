0489076c78b7709bdc8ee24c1444e64b0a7c2669
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-05-02 17:41:50 +0200

    Update examples

---
+++ Examples/ExampleWord01-CreateDocs.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+Import-Module .\PSWriteOffice.psd1 -Force
+
+$Document = New-OfficeWord -FilePath $PSScriptRoot\Documents\Test.docx
+
+New-OfficeWordText -Document $Document -Text 'This is a test, very big test ', 'and this should be bold' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
+
+New-OfficeWordText -Document $Document -Text 'This is a test, very big test', 'ooops' -Color Blue, Gold -Alignment Right
+
+$Paragraph = New-OfficeWordText -Document $Document -Text 'Centered' -Color Blue, Gold -Alignment Center -ReturnObject
+
+New-OfficeWordText -Document $Document -Text ' Attached to existing paragraph', ' continue' -Paragraph $Paragraph -Color Blue
+
+Save-OfficeWord -Document $Document -Show
---
+++ Examples/ExampleWord02-OpenWord.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+Import-Module .\PSWriteOffice.psd1 -Force
+
+$Document = Get-OfficeWord -FilePath $PSScriptRoot\Documents\Test.docx
+
+New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
+New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
+New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
+
+Save-OfficeWord -Document $Document -Show -FilePath $PSScriptRoot\Documents\Test6.docx
