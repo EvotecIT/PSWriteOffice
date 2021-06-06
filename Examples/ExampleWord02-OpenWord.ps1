24eac3f96abf8c35c035949bc614a36f709e7a74
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-06 11:12:47 +0200

    Small update to example

--- Examples/ExampleWord02-OpenWord.ps1
+++ Examples/ExampleWord02-OpenWord.ps1
@@ -5,6 +5,6 @@ $Document = Get-OfficeWord -FilePath $PSScriptRoot\Documents\Test.docx
 
 New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
 New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
-New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
+New-OfficeWordText -Document $Document -Text 'Add more things!', ' a bit more  with bold' -Bold $null, $true -Underline $null, $true -Space Preserve, Preserve
 
-Save-OfficeWord -Document $Document -Show -FilePath $PSScriptRoot\Documents\Test6.docx
+Save-OfficeWord -Document $Document -Show -FilePath $PSScriptRoot\Documents\Test6.docx -Retry 1
