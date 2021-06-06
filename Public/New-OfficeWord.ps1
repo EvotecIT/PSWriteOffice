cbaab5ee1d4a9578ec27e75ca184d0023aee1f0d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-06 11:28:20 +0200

    Improvement of New-OfficeWord to retry and save to temp file

--- Public/New-OfficeWord.ps1
+++ Public/New-OfficeWord.ps1
@@ -2,16 +2,31 @@
     [cmdletBinding()]
     param(
         [string] $FilePath,
-        [switch] $AutoSave
+        [switch] $AutoSave,
+        [int] $Retry = 2
     )
-    try {
-        $WordDocument = [DocumentFormat.OpenXml.Packaging.WordprocessingDocument]::Create($FilePath, [DocumentFormat.OpenXml.WordprocessingDocumentType]::Document, $AutoSave.IsPresent)
-    } catch {
-        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-            throw
-        } else {
-            Write-Warning "New-OfficeWord - Couldn't create new Word Document at $FilePath. Error: $($_.Exception.Message)"
-            return
+    $Saved = $false
+    $Count = 0
+    while ($Count -le $Retry -and $Saved -eq $false) {
+        $Count++
+        try {
+            $WordDocument = [DocumentFormat.OpenXml.Packaging.WordprocessingDocument]::Create($FilePath, [DocumentFormat.OpenXml.WordprocessingDocumentType]::Document, $AutoSave.IsPresent)
+            $Saved = $true
+        } catch {
+            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                throw
+            } else {
+                Write-Warning "New-OfficeWord - Couldn't create new Word Document at $FilePath. Error: $($_.Exception.Message)"
+            }
+        }
+        if (-not $Saved) {
+            if ($Retry -ge $Count) {
+                $FilePath = [io.path]::GetTempFileName().Replace('.tmp', '.docx')
+                Write-Warning -Message "New-OfficeWord - Couldn't save using provided file name, retrying with $FilePath"
+            } else {
+                Write-Warning -Message "New-OfficeWord - Couldn't save using provided file name. Run out of retries ($Count / $Retry)."
+                return
+            }
         }
     }
     $null = $WordDocument.AddMainDocumentPart();
