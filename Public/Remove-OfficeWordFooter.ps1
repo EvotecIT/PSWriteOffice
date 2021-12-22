0b388cc4018bcdb33a72df08e6670d5c81cddc36
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-22 09:49:01 +0100

    Added 2 new commands

---
+++ Public/Remove-OfficeWordFooter.ps1
@@ -0,0 +1,11 @@
+﻿function Remove-OfficeWordFooter {
+    [cmdletBinding()]
+    param(
+        [DocumentFormat.OpenXml.Packaging.WordprocessingDocument] $Document
+    )
+    try {
+        [OfficeImo.Footers]::RemoveFooters($Document)
+    } catch {
+        Write-Warning -Message "Remove-OfficeWordFooter - Couldn't remove footer. Error: $($_.Exception.Message)"
+    }
+}
---
+++ Public/Remove-OfficeWordHeader.ps1
@@ -0,0 +1,11 @@
+﻿function Remove-OfficeWordHeader {
+    [cmdletBinding()]
+    param(
+        [DocumentFormat.OpenXml.Packaging.WordprocessingDocument] $Document
+    )
+    try {
+        [OfficeImo.Headers]::RemoveHeaders($Document)
+    } catch {
+        Write-Warning -Message "Remove-OfficeWordHeader - Couldn't remove footer. Error: $($_.Exception.Message)"
+    }
+}
