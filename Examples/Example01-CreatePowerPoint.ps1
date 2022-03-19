88bab0a0407bd523aa45a8f11c6a81a1a165f7c1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-19 10:59:37 +0100

    Not ready

--- Examples/Example01-CreatePowerPoint.ps1
+++
@@ -1,9 +0,0 @@
-﻿Clear-Host
-Import-Module .\PSWriteOffice.psd1 -Force
-
-$PowerPoint = New-OfficePowerPoint -FilePath $PSScriptRoot\Documents\PowerPoint.pptx
-
-
-$PowerPoint.PresentationPart
-
-Save-OfficePowerPoint -PowerPoint $PowerPoint -Show
