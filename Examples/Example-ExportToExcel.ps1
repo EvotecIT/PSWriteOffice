0e3d8c98faea6a1f9cbb8a5df1d3e9f1f2f9b253
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-19 14:27:54 +0100

    Example for Export-Excel

---
+++ Examples/Example-ExportToExcel.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+Import-Module .\PSWriteOffice.psd1 -Force
+
+$Data1 = Get-Process | Select-Object -First 15
+$Objects = @(
+    [PSCustomObject] @{ Test = 1; DateTime = (Get-Date); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'string' }
+    [PSCustomObject] @{ Test = 1; }
+    [PSCustomObject] @{ Test = 3; DateTime = (Get-Date).AddDays(1); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'string' }
+)
+
+$Objects | Export-OfficeExcel -FilePath "$PSScriptRoot\Documents\ExportToExcel.xlsx" -WorksheetName "Sheet3" 
+$Data1 | Export-OfficeExcel -FilePath "$PSScriptRoot\Documents\ExportToExcel.xlsx" -WorksheetName "Sheet4" -Show
