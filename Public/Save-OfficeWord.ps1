73357cc9c7e7e7400245204ca41cc0f59d531109
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-27 22:59:08 +0200

    Support for new officeimo

--- Public/Save-OfficeWord.ps1
+++ Public/Save-OfficeWord.ps1
@@ -1,7 +1,7 @@
 ﻿function Save-OfficeWord {
     [cmdletBinding()]
     param(
-        [alias('WordDocument')][DocumentFormat.OpenXml.Packaging.WordprocessingDocument] $Document,
+        [alias('WordDocument')][OfficeIMO.Word.WordDocument] $Document,
         [switch] $Show,
         [string] $FilePath,
         [int] $Retry = 2
@@ -10,8 +10,6 @@
         Write-Warning "Save-OfficeWord - Couldn't save Word Document. Document is null."
         return
     }
-    $Saved = $false
-    $Count = 0
     if (-not $Document.FilePath -and -not $FilePath) {
         if ($PSBoundParameters.ErrorAction -eq 'Stop') {
             throw
@@ -20,79 +18,34 @@
             return
         }
     }
-    while ($Count -le $Retry -and $Saved -eq $false) {
-        $Count++
-        if ($FilePath) {
-            # File path was given so we use SaveAs
+    if ($FilePath) {
+        # File path was given so we use SaveAs
+        try {
+            $null = $Document.Save($FilePath, $Show.IsPresent)
+        } catch {
+            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                throw
+            } else {
+                Write-Warning "Save-OfficeWord - Couldn't save $FilePath. Error: $($_.Exception.Message)"
+            }
+        } finally {
+            #$NewDocument.Dispose()
+            #$Document.Dispose()
+        }
+    } else {
+        if (-not $Document.AutoSave) {
             try {
-                $NewDocument = $Document.SaveAs($FilePath)
-                $Saved = $true
+                $Document.Save($Show.IsPresent)
             } catch {
                 if ($PSBoundParameters.ErrorAction -eq 'Stop') {
                     throw
                 } else {
-                    Write-Warning "Save-OfficeWord - Couldn't save $FilePath. Error: $($_.Exception.Message)"
+                    Write-Warning "Save-OfficeWord - Couldn't save $($Document.FilePath) Error: $($_.Exception.Message)"
                 }
-            }
-
-        } else {
-            $FilePath = $Document.FilePath
-            if (-not $Document.AutoSave) {
-                try {
-                    $Document.Save()
-                    $Saved = $true
-                } catch {
-                    if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-                        throw
-                    } else {
-                        Write-Warning "Save-OfficeWord - Couldn't save $FilePath. Error: $($_.Exception.Message)"
-                    }
-                }
-            }
-        }
-        if (-not $Saved) {
-            if ($Retry -ge $Count) {
-                $FilePath = [io.path]::GetTempFileName().Replace('.tmp', '.docx')
-                Write-Warning -Message "Save-OfficeWord - Couldn't save using provided file name, retrying with $FilePath"
-            } else {
-                Write-Warning -Message "Save-OfficeWord - Couldn't save using provided file name. Run out of retries ($Count / $Retry)."
+            } finally {
+                #$Document.Dispose()
             }
         }
     }
-    try {
-        $Document.Close()
-    } catch {
-        if ( $_.Exception.InnerException.Message -eq "Memory stream is not expandable.") {
-            # we swallow this exception because it only fails on PS 7.
-        } else {
-            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-                throw
-            } else {
-                Write-Warning "Save-OfficeWord - Couldn't close document for $FilePath. Error: $($_.Exception.Message)"
-            }
-        }
-    }
-
-    if ($NewDocument) {
-        try {
-            $NewDocument.Close()
-        } catch {
-            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-                throw
-            } else {
-                Write-Warning "Save-OfficeWord - Couldn't close document for $FilePath. Error: $($_.Exception.Message)"
-            }
-        }
-    }
-    if ($Show) {
-        try {
-            Invoke-Item -LiteralPath $FilePath -ErrorAction Stop
-        } catch {
-            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-                throw
-            } else {
-                Write-Warning "Save-OfficeWord - Couldn't open $FilePath. Error: $($_.Exception.Message)"
-            }
-        }
-    }
+    $Document.Dispose()
 }
