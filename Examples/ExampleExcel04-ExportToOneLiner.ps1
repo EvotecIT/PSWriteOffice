05cf7f4485fdfcee51981da4dd8026a115025eff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-10 17:35:12 +0200

    Updated example

--- Examples/ExampleExcel04-ExportToOneLiner.ps1
+++ Examples/ExampleExcel04-ExportToOneLiner.ps1
@@ -2,6 +2,3 @@
 
 $ProcessList = Get-Process | Select-Object -First 50
 Export-OfficeExcel -FilePath $PSScriptRoot\Documents\Test5.xlsx -WorksheetName 'Contact3' -DataTable $ProcessList  #-Show
-
-# cmdlet from PSWriteExcel for speed comparison
-# ConvertTo-Excel -FilePath $PSScriptRoot\Documents\Test15.xlsx -ExcelWorkSheetName 'Contact3' -DataTable $ProcessList #-OpenWorkBook
