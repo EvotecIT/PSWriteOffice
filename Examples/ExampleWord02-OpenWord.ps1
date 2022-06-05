e7703122c5ad581a5619e12dac15a43093ef9702
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-05 12:19:27 +0200

    Fixees example

--- Examples/ExampleWord02-OpenWord.ps1
+++ Examples/ExampleWord02-OpenWord.ps1
@@ -1,10 +1,12 @@
 ﻿Clear-Host
 Import-Module .\PSWriteOffice.psd1 -Force
 
-$Document = Get-OfficeWord -FilePath $PSScriptRoot\Documents\Test.docx
+if (Test-Path -LiteralPath "$PSScriptRoot\Documents\Test.docx") {
+    $Document = Get-OfficeWord -FilePath $PSScriptRoot\Documents\Test.docx
 
-New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, 'Dotted'
-New-OfficeWordText -Document $Document -Text 'Add more things!', ' Ok?' -Bold $true -Underline $null, 'Double'
-New-OfficeWordText -Document $Document -Text 'Add more things!', ' a bit more  with bold' -Bold $null, $true -Underline Dash -Color Red
+    New-OfficeWordText -Document $Document -Text 'Add more things!' -Bold $null, $true -Underline $null, 'Dotted'
+    New-OfficeWordText -Document $Document -Text 'Add more things!', ' Ok?' -Bold $true -Underline $null, 'Double'
+    New-OfficeWordText -Document $Document -Text 'Add more things!', ' a bit more  with bold' -Bold $null, $true -Underline Dash -Color Red
 
-Save-OfficeWord -Document $Document -Show -FilePath $PSScriptRoot\Documents\Test6.docx -Retry 1
+    Save-OfficeWord -Document $Document -Show -FilePath $PSScriptRoot\Documents\Test6.docx -Retry 1
+}
