0d995ce44357ace487887519cc894a298a3e1990
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-16 21:49:33 +0200

    Playing with colors

---
+++ Examples/Example03-CreateDocWithTable02.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+Import-Module .\PSWriteOffice.psd1 -Force
+
+$Document = New-OfficeWord -FilePath $PSScriptRoot\Documents\TestTable.docx
+New-OfficeWordText -Document $Document -Text 'This is a test, very big test ', 'and this should be bold' -Bold $null, $true -Underline $null, 'Double'
+
+$DataTable = @(
+    [PSCustomObject] @{ Test = 1; DateTime = (Get-Date); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'string' }
+    [PSCustomObject] @{ Test = 1; }
+    [PSCustomObject] @{ Test = 3; DateTime = (Get-Date).AddDays(1); TimeSpan = (New-TimeSpan -Minutes 10); TestString = 'Nope' }
+)
+
+$Table = New-OfficeWordTable -Document $Document -DataTable $DataTable -TableLayout Autofit #-Style GridTable1LightAccent1
+foreach ($Cell in $Table.Rows[0].Cells) {
+    $Cell.Paragraphs[0].Color = '#FF0000'
+    $Cell.Paragraphs[0].Highlight = [DocumentFormat.OpenXml.Wordprocessing.HighlightColorValues]::Magenta
+}
+
+New-OfficeWordText -Document $Document
+New-OfficeWordText -Document $Document -Text "Another table below" -Bold $true -Color RedBerry -Alignment Center
+
+New-OfficeWordTable -Document $Document -DataTable $DataTable -TableLayout Autofit -Style GridTable2 -SkipHeader
+
+Save-OfficeWord -Document $Document -Show
