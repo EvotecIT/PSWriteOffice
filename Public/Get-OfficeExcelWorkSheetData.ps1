b6705545cb2ba369e06cefa26f37c4baf981bdd5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-13 15:22:40 +0200

    Added command to read data

---
+++ Public/Get-OfficeExcelWorkSheetData.ps1
@@ -0,0 +1,31 @@
+﻿function Get-OfficeExcelWorkSheetData {
+    [cmdletBinding()]
+    param(
+        [ClosedXML.Excel.IXLWorksheet] $WorkSheet
+    )
+
+    $HeaderNames = [System.Collections.Generic.List[string]]::new()
+    foreach ($Cell in $WorkSheet.RangeUsed().Row(1).Cells()) {
+        # There is a bug in closed xml that causes
+        # reported on https://github.com/ClosedXML/ClosedXML/issues/1852
+        if ($Cell.InnerText -ne "") {
+            $Name = $Cell.InnerText
+        } else {
+            $Name = "NoName$($Cell.Address)"
+        }
+        # We need to check for header duplicates, if someone made a mistake
+        if ($HeaderNames.Contains($Name)) {
+            $Name = $Name + $($Cell.Address)
+        }
+        $HeaderNames.Add($Name)
+    }
+    $LastRowUsed = $WorkSheet.RangeUsed().RowCount()
+
+    foreach ($Row in $WorkSheet.RangeUsed().Rows(2, $LastRowUsed)) {
+        $RowData = [ordered] @{}
+        for ($i = 0; $i -lt $HeaderNames.Count; $i++) {
+            $RowData[$HeaderNames[$i]] = $Row.Cells($i + 1).CachedValue
+        }
+        [PSCustomObject] $RowData
+    }
+}
