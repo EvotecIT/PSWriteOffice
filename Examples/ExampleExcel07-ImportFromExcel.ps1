6d4b6404b6e44d876a0b84576fa932595f782929
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-13 16:09:03 +0200

    Added example for import

---
+++ Examples/ExampleExcel07-ImportFromExcel.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSWriteOffice.psd1 -Force
+
+$FilePath = "$PSScriptRoot\Documents\Test5.xlsx"
+
+$ImportedData1 = Import-OfficeExcel -FilePath $FilePath
+$ImportedData1 | Format-Table
+
+$FilePath = "$PSScriptRoot\Documents\Excel.xlsx"
+
+$ImportedData2 = Import-OfficeExcel -FilePath $FilePath -WorkSheetName 'Contact3'
+$ImportedData2 | Format-Table
