f16a3e1c8e26a6eb4460bac436ee0010c7f53f09
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-11 11:04:15 +0100

    Add another example

---
+++ Examples/ExampleWord10-CreateDocsWithTabs.ps1
@@ -0,0 +1,14 @@
+﻿#Clear-Host
+Import-Module .\PSWriteOffice.psd1 -Force
+
+$Document = New-OfficeWord -FilePath $PSScriptRoot\Documents\BasicDocument.docx
+
+New-OfficeWordText -Document $Document -Text 'This is a test, very big test ', 'and this should be bold' -Bold $null, $true -Underline Dash, $null
+
+New-OfficeWordText -Document $Document -Text 'This is a test, \t\t very big test ', 'and this should be bold' -Bold $null, $true -Underline Dash, $null
+
+New-OfficeWordText -Document $Document -Text "This is a test, `t`t very big test ", 'and this should be bold' -Bold $null, $true -Underline Dash, $null
+
+$p3 = New-OfficeWordText -Document $Document -Bold 1, 0 -Underline $null, Dash -Text "We are ", "`t`tJohn Doe and Jane Doe`t`t" -ReturnObject
+
+Save-OfficeWord -Document $Document -Show
