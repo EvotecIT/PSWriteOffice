1188423a29186009b202221a562972893d42893f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-19 18:59:14 +0200

    Update configuration of project

---
+++ Sources/PSWriteOffice.TestingGround/Program.cs
@@ -0,0 +1,27 @@
+﻿using PSWriteOffice;
+using ShapeCrawler;
+
+namespace PSWriteOffice.TestingGround;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+
+        ExampleCreatePresentation();
+    }
+
+    public static void ExampleCreatePresentation() {
+        // create a new presentation
+        var pres = new Presentation();
+
+        var shapes = pres.Slides[0].Shapes;
+
+        // add new shape
+        shapes.AddRectangle(x: 50, y: 60, width: 100, height: 70);
+        var addedShape = shapes.Last();
+
+        addedShape.TextBox!.Text = "Hello World!";
+
+        pres.SaveAs("my_pres.pptx");
+    }
+}
