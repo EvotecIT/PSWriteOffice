0f2b9683626c41bdc9dfbde92a7ac0075010f955
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-28 09:35:20 +0100

    Add powerpoint presentation option

---
+++ Examples/ExamplePowerPoint01-AddSlides.ps1
@@ -0,0 +1,22 @@
+﻿Clear-Host
+#Import-Module .\PSWriteOffice.psd1 -Force
+
+$Presentation = New-OfficePowerPoint -FilePath "$PSScriptRoot\Documents\ExamplePowerPoint1.pptx"
+
+$Presentation.Slides.AddEmptySlide([ShapeCrawler.SlideLayoutType]::Title)
+
+Write-Color -Text $Presentation.Slides.Count -Color Green
+
+# Get the shapes collection from the first slide
+$shapes = $Presentation.Slides[0].Shapes
+
+# Add a new rectangle shape
+$shapes.AddRectangle(50, 60, 100, 70)
+
+$shapes.Item(0).TextBox.Text = "Hello World!"
+
+$Presentation.Slides[1].Shapes | Format-Table
+
+$Presentation.Slides[1].Shapes[0].TextBox.Text = "This is my title"
+
+Save-OfficePowerPoint -Presentation $Presentation -Show
