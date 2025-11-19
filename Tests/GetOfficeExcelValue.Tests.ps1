b4dc7962945fa7761711901799f96dd3b30486b0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-11-19 12:14:34 +0100

    refactor(tests): 🗑️ remove obsolete Excel and Word test files
    * Deleted outdated test files for Excel and Word cmdlets.
    * Cleans up the test suite as part of the PSWriteOffice reboot.

--- Tests/GetOfficeExcelValue.Tests.ps1
+++
@@ -1,9 +0,0 @@
-Describe 'Get-OfficeExcelValue cmdlet' {
-    It 'returns cell object for given coordinates' {
-        $workbook = New-OfficeExcel
-        $worksheet = New-OfficeExcelWorkSheet -Workbook $workbook -WorksheetName 'Sheet1' -Option Replace
-        $worksheet.Cell(2, 3).Value = 'data'
-        $cell = Get-OfficeExcelValue -Worksheet $worksheet -Row 2 -Column 3
-        $cell.Value | Should -Be 'data'
-    }
-}
--- Tests/NewOfficeExcelValue.Tests.ps1
+++
@@ -1,8 +0,0 @@
-Describe 'New-OfficeExcelValue cmdlet' {
-    It 'writes value to a cell' {
-        $workbook = New-OfficeExcel
-        $worksheet = New-OfficeExcelWorkSheet -Workbook $workbook -WorksheetName 'Sheet1' -Option Replace
-        New-OfficeExcelValue -Worksheet $worksheet -Row 3 -Column 2 -Value 'hello'
-        $worksheet.Cell(3,2).Value | Should -Be 'hello'
-    }
-}
