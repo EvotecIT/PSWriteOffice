3ba8b2b9347b2506a927d9382c454ef43e79052a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-08-12 09:04:08 +0200

    update

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -85,7 +85,7 @@ Build-Module -ModuleName 'PSWriteOffice' -RunMode $RunMode {
         SignModule                        = $true
         MergeModuleOnBuild                = $true
         MergeFunctionsFromApprovedModules = $true
-        CertificateThumbprint             = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+        CertificateThumbprint             = '92e95fb58effa6a4a75e77a33cdd6bfe6dd30f1a'
         ResolveBinaryConflicts            = $true
         ResolveBinaryConflictsName        = 'PSWriteOffice'
         NETProjectName                    = 'PSWriteOffice'
